37a2415bc3acf0e0f0219098c2e262463d9161ae
DLee0102 <1225390750@qq.com>
2023-01-30 12:14:19 +0800

    添加注释，修改了一些细节

--- src/Dxx_tool_v0.1/Core.cpp
+++ src/Dxx_tool_v0.1/Core.cpp
@@ -1,9 +1,9 @@
 #include "Core.h"
 
 namespace DXXTL {
-    Excelops::Excelops(std::string obj_)
+    Excelops::Excelops(std::string obj_, int temp_start_row_, int total_start_row_)
     {
-        if (init(obj_)) {
+        if (init(obj_, temp_start_row_, total_start_row_)) {
             std::cout << "                    初始化完成..." << std::endl;
         }
     }
@@ -23,11 +23,17 @@ namespace DXXTL {
         return start_row;
     }
 
-    int Excelops::getCollength(OpenXLSX::XLWorksheet wks)
+    /**
+     * @brief 以除表头外第一行内容为参照确定列数
+     * 
+     * @param wks 
+     * @return int 
+     */
+    int Excelops::getCollength(OpenXLSX::XLWorksheet wks, int start_row)
     {
         int col_length = 0;
         std::vector<OpenXLSX::XLCellValue> readValues;
-        for (auto& row : wks.rows(3, total_length)) {
+        for (auto& row : wks.rows(start_row, total_length)) {
             readValues = row.values();
             for (int i = 0; i < readValues.size(); i++) {
                 if (readValues[i] == "") {break;}
@@ -46,8 +52,11 @@ namespace DXXTL {
         }
     }
 
-    bool Excelops::init(std::string obj_)
+    bool Excelops::init(std::string obj_, int temp_start_row_, int total_start_row_)
     {
+        temp_start_row = temp_start_row_;
+        total_start_row = total_start_row_;
+
         total_path = "./软件学院2021级青年大学习学习情况记录.xlsx";
         temp_path = "./学习用户明细.xlsx";
 
@@ -59,7 +68,7 @@ namespace DXXTL {
 
         temp_length = getRowlength(wks_temp, 1);
         total_length = getRowlength(wks_total, 1);
-        col_length = getCollength(wks_total);
+        col_length = getCollength(wks_total, total_start_row);
         std::cout << "                    OpenXLSX框架初始化完成..." << std::endl;
 
         writeValues_total.resize(total_length - 2);
@@ -90,13 +99,13 @@ namespace DXXTL {
         }
     }
 
-    void Excelops::loadData(int start_row)
+    void Excelops::loadData()
     {
         std::cout << "                    正在填入数据，请稍等..." << std::endl;
         std::cout << "                    填入数据量为：" << " ";
         std::cout << writeValues_total.size() << std::endl;
         for (int i = 0; i < writeValues_total.size(); i++) {
-            wks_total.cell(start_row + i, col_length + 1).value() = writeValues_total[i];
+            wks_total.cell(total_start_row + i, col_length + 1).value() = writeValues_total[i];
             // std::cout << writeValues_total[i] << std::endl;
         }
 
--- src/Dxx_tool_v0.1/Core.h
+++ src/Dxx_tool_v0.1/Core.h
@@ -8,22 +8,30 @@ namespace DXXTL{
     class Excelops
     {
     public:
-        Excelops(std::string obj_);
-        int getRowlength(OpenXLSX::XLWorksheet wks, int start_row);
-        int getCollength(OpenXLSX::XLWorksheet wks);
-        bool initWritevector(std::vector<OpenXLSX::XLCellValue>& writeValues, int length);
-        bool init(std::string obj_);
-        void fillWriteValues(int checkedNamecol, int loadedNamecol);
-        void loadData(int start_row);
-        ~Excelops();
+        Excelops(std::string obj_, int temp_start_row_, int total_start_row_);    // 初始化框架
+        int getRowlength(OpenXLSX::XLWorksheet wks, int start_row);    // 获取行长度
+        int getCollength(OpenXLSX::XLWorksheet wks, int start_row);    // 获取列长度
+        bool initWritevector(std::vector<OpenXLSX::XLCellValue>& writeValues, int length);    // 初始化写入向量
+        bool init(std::string obj_, int temp_start_row_, int total_start_row_);    // 初始化框架
+        void fillWriteValues(int checkedNamecol, int loadedNamecol);    // 将数据读取到写入向量中
+        void loadData();    // 将写入向量中的数据写到指定表中
+        ~Excelops();    // 关闭表格文件
     
     private:
+        /**
+         * @brief 待写入的文件
+         * 
+         */
         std::string total_path;
         OpenXLSX::XLDocument doc_total;
         OpenXLSX::XLWorksheet wks_total;
         std::vector<OpenXLSX::XLCellValue> readValues_total;
         std::vector<OpenXLSX::XLCellValue> writeValues_total;
 
+        /**
+         * @brief 待读取的文件
+         * 
+         */
         std::string temp_path;
         OpenXLSX::XLDocument doc_temp;
         OpenXLSX::XLWorksheet wks_temp;
@@ -32,5 +40,9 @@ namespace DXXTL{
         int temp_length;
         int total_length;
         int col_length;
+
+        // 数据从第几行开始
+        int temp_start_row;
+        int total_start_row;
     };
 }
